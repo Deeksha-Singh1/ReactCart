6b3d872c26a5973657a9d407c7ded2069a64cfe5
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-06-23 01:26:35 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Navbar from './component/Navbar';
+import About from './pages/About';
+import Home from './pages/Home';
 function App() {
   return (
-    <div className='App'>My app</div>
+    <>
+      <Router >
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+        </Routes>
+      </Router>
+    </>
+   
   );
 }
 
 export default App;
+ 
